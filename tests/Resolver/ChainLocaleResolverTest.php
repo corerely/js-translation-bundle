7f56b1f152bc62666bdf0f0ca29e5c145acfb8c8
Andrii Oprysko <anjey.link@gmail.com>
2021-10-20 15:35:23 +0300

    refactor: make ChainResolver check if locale is supported

--- tests/Resolver/ChainLocaleResolverTest.php
+++ tests/Resolver/ChainLocaleResolverTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 
 class ChainLocaleResolverTest extends TestCase
 {
-    public function testResolveReturnLocaleFromFirstResolverThatReturnsTruthyValue()
+    public function testResolveReturnLocaleFromFirstResolverThatReturnSupportedLocale()
     {
         $locale = 'en';
 
@@ -22,7 +22,7 @@ class ChainLocaleResolverTest extends TestCase
         $resolver3 = $this->createMock(LocaleResolverInterface::class);
         $resolver3->expects($this->never())->method('resolve');
 
-        $chainResolver = (new ChainLocaleResolver())->addResolver($resolver1)->addResolver($resolver2)->addResolver($resolver3);
+        $chainResolver = (new ChainLocaleResolver(['en']))->addResolver($resolver1)->addResolver($resolver2)->addResolver($resolver3);
 
         $this->assertEquals($locale, $chainResolver->resolve());
     }
@@ -32,7 +32,24 @@ class ChainLocaleResolverTest extends TestCase
         $resolver = $this->createMock(LocaleResolverInterface::class);
         $resolver->expects($this->once())->method('resolve')->willReturn(null);
 
-        $chainResolver = (new ChainLocaleResolver())->addResolver($resolver);
+        $chainResolver = (new ChainLocaleResolver(['uk']))->addResolver($resolver);
+
+        $this->expectException(\LogicException::class);
+        $chainResolver->resolve();
+    }
+
+    public function testResolveThrownExceptionIfAllResolvedIsNotSupported()
+    {
+        $resolver1 = $this->createMock(LocaleResolverInterface::class);
+        $resolver1->expects($this->once())->method('resolve')->willReturn('uk');
+
+        $resolver2 = $this->createMock(LocaleResolverInterface::class);
+        $resolver2->expects($this->once())->method('resolve')->willReturn(null);
+
+        $resolver3 = $this->createMock(LocaleResolverInterface::class);
+        $resolver3->expects($this->once())->method('resolve')->willReturn('en');
+
+        $chainResolver = (new ChainLocaleResolver(['sv']))->addResolver($resolver1)->addResolver($resolver2)->addResolver($resolver3);
 
         $this->expectException(\LogicException::class);
         $chainResolver->resolve();
